5d59d1111f80d085aba819eca60d1a2c7297ea73
hkkong <konguuu@gmail.com>
2024-09-08 20:54:23 +0900

    의존 오브젝트 DI 적용

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,15 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Objects;
 
 
 public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     public String hello(String name) {
         SimpleHelloService helloService = new SimpleHelloService();
